3eeb13ce662272cc8cd199c5672bdb1c9c6b5d9e
davidadediji <davidadediji@gmail.com>
2023-03-28 15:51:44 +0100

    chore:fix background image not displaying

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,7 @@
+import { url } from 'inspector'
 import React from 'react'
 import VisaLogo from '../assets/icons/visalogo.svg'
+import Background from '../assets/images/visacard.svg'
 
 type CardProps ={
 	name:string
@@ -8,7 +10,10 @@ type CardProps ={
 }
 export default function Card({name, cardNo, expiryDate}:CardProps) {
   return (
-		<section className='flex flex-col justify-between bg-cover bg-openpay-image w-[205.48px] h-[108.13px] pl-3 text-[#fff] py-2'>
+		<section
+			className='flex flex-col justify-between w-[205.48px] h-[108.13px] pl-3 text-[#fff] py-2'
+			style={{ backgroundImage: `url(${Background})` }}
+		>
 			<div>
 				<p className='text-[8.77px]'>Name</p>
 				<p className='text-[13.16px]'>{name}</p>
@@ -19,7 +24,7 @@ export default function Card({name, cardNo, expiryDate}:CardProps) {
 					<p className='text-[8.04px]'>{expiryDate}</p>
 				</div>
 				<div>
-					<img src={VisaLogo} alt="" />
+					<img src={VisaLogo} alt='' />
 				</div>
 			</div>
 		</section>
